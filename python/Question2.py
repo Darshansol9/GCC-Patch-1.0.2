d87ec55d77091e60f6f595074c43677b908526dc
Darshansol9 <56557988+Darshansol9@users.noreply.github.com>
2019-10-14 17:44:44 -0400

    Add files via upload

--- python/Question2.py
+++ python/Question2.py
@@ -1,5 +1,17 @@
 # modify this function, and create other functions below as you wish
 def question02(risk, bonus, trader):
+    total_profit = 0
+    for t in range(0,len(trader)):
+        curr_profit = 0
+        for r in range(0,len(risk)):
+            if(trader[t] < risk[r]):
+                continue
+            else:
+                if(curr_profit < bonus[r]):
+                    curr_profit = bonus[r]
+                    
+        total_profit += curr_profit
+                    
     # modify and then return the variable below
-    answer = -5
-    return answer
+    
+    return total_profit
